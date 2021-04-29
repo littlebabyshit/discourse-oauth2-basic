f8c9de200307719d8937e6900d865b155d8cb471
littlebabyshit <935396509@qq.com>
2021-04-30 00:42:46 +0800

    删除解析token

--- plugin.rb
+++ plugin.rb
@@ -42,7 +42,17 @@ class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
     redirect client.authorize_url(params)
     # 源码
   end
-
+  protected
+  def build_access_token
+    params = {
+      'appid'        => client.id,
+      'secret'       => client.secret,
+      'code'         => request.params['code'],
+      'grant_type'   => 'authorization_code',
+      'redirect_uri' => callback_url
+      }.merge(token_params.to_hash(symbolize_keys: true))
+    client.get_token(params, deep_symbolize(options.auth_token_params))
+  end
   def callback_url
     Discourse.base_url_no_prefix + script_name + callback_path
   end
